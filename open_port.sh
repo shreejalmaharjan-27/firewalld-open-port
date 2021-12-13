522e490900a38fa6d4029137e1c440c27704fbfa
Shreejal Maharjan <shreejal@rafled.com>
2021-12-13 16:54:39 +0545

    add check if command exists

--- open_port.sh
+++ open_port.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 # Author : Shreejal Maharjan
-
-echo "What port do you want to open?"
-read PORT
-sudo firewall-cmd --zone=public --add-port=$PORT/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=$PORT/udp --permanent
+if which firewall-cmd >/dev/null; then
+        echo "What port do you want to open?"
+        read PORT
+        sudo firewall-cmd --zone=public --add-port=$PORT/tcp --permanent  >/dev/null
+        sudo firewall-cmd --zone=public --add-port=$PORT/udp --permanent  >/dev/null
+        sudo firewall-cmd --reload
+else
+    echo please installed firewalld
+fi
